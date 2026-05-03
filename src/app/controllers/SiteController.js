7903ddefd5968e89ab18712c0e228df2883ed43f
ThanDinh <thankt26k4@gmail.com>
2026-05-03 22:47:19 +0700

    MVC Routes & Controllers

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,15 @@
+class SiteController {
+
+    // [GET] /
+    index(req, res) {
+        res.render('Home')
+    }
+
+    // [GET] /search
+    search(req, res) {
+        res.render('Search')
+    }
+
+}
+
+module.exports = new SiteController
